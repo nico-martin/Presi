76f4c5b7c64e1ee8a02437f4b224fbdaf79c0ab1
Nico Martin <mail@nico.dev>
2026-08-31 14:24:03 +0200

    improved notes and exports

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -256,7 +256,7 @@ const writePackageJson = async () => {
     `${JSON.stringify(
       {
         name: "presi-js",
-        version: "0.0.16",
+        version: "0.0.17",
         description: "A modern presentation framework",
         type: "module",
         author: "Nico Martin <mail@nico.dev>",
